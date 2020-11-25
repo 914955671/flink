cc7dc6eb3efec80e7ece75a81c493f07a02ec830
relei1234@163.com <relei1234>
2020-11-25 22:20:59 +0800

    测试

--- src/main/scala/com/pxd/TableAPI/T2.scala
+++ src/main/scala/com/pxd/TableAPI/T2.scala
@@ -1,9 +1,11 @@
 package com.pxd.TableAPI
 import org.apache.flink.api.java.ExecutionEnvironment
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api._
 import org.apache.flink.table.api.java.BatchTableEnvironment
-import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, OldCsv, Schema}
+import org.apache.flink.table.types.DataType
 
 
 object T2 {
@@ -14,6 +16,7 @@ object T2 {
 
     //2. 定义表环境
     val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+/*
 
     //2.1 基于老版本planner的流处理
     val settings: EnvironmentSettings = EnvironmentSettings.newInstance()
@@ -39,10 +42,41 @@ object T2 {
       .useBlinkPlanner()
       .inBatchMode()
       .build()
-    val blinkBatchTableEnv: StreamTableEnvironment = TableEnvironment.create(blinkBatchSettings)
+    val blinkBatchTableEnv: TableEnvironment = TableEnvironment.create(blinkBatchSettings)
+*/
+
+    //3. 连接外部系统，读取数据，注册表
+    //3.1 读取文件
+    val filePath = "D:\\IDEA\\Project\\flink\\src\\main\\scala\\com\\pxd\\text.txt"
+
+    tableEnv.connect(new FileSystem().path(filePath))
+      .withFormat(new Csv())
+      .withSchema(new Schema()
+                .field("field_1",DataTypes.STRING())
+                .field("field_2",DataTypes.INT())
+                .field("field_3",DataTypes.DOUBLE())
+      ).createTemporaryTable("inputTable")
+
+    //3.2 从kafka中读取数据
+    tableEnv.connect(new Kafka()
+      .version("2.6.0")
+      .topic("test")
+      .property("zookeeper.connect","localhost:2181")
+      .property("boostrap.servers","localhost:9092")
+    ).withFormat(new Csv())
+      .withSchema(new Schema()
+        .field("field_1",DataTypes.STRING())
+        .field("field_2",DataTypes.INT())
+        .field("field_3",DataTypes.DOUBLE())
+      ).createTemporaryTable("KafkaTable")
 
 
+    val inputTable: Table = tableEnv.from("inputTable")
+    val KafkaTable: Table = tableEnv.from("KafkaTable")
 
+//    inputTable.toAppendStream[(String, Int, Double)].print()
+    KafkaTable.toAppendStream[(String, Int, Double)].print()
+    env.execute("Table API Test")
 
   }
 }
